c3a77dee52e04ea9a8aa00e6874845cb816c2073
rumeysagelgi <79201800+rumeysagelgi@users.noreply.github.com>
2024-05-17 18:45:48 +0300

    Typo fixed - Update seo.js

--- components/seo.js
+++ components/seo.js
@@ -6,7 +6,7 @@ const SEO = () => {
       <meta charSet="utf-8" />
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Currency Converter Application</title>
+      <title>Currency Converter</title>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link href="https://fonts.googleapis.com/css2?family=Junge&display=swap" rel="stylesheet" />
